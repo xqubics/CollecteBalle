c056ed27005be99e68400cbef5e6ec2b31194d23
Dada462 <danut462@gmail.com>
2023-02-20 11:50:44 +0100

    marqueurs visuels pour positionnement

--- ws/src/robot_control/robot_control/joystick.py
+++ ws/src/robot_control/robot_control/joystick.py
@@ -93,26 +93,25 @@ class MinimalPublisher(Node):
     def __init__(self):
         super().__init__('minimal_publisher')
         self.publisher_ = self.create_publisher(Twist, 'demo/cmd_vel', 10)
-        timer_period = 0.5  # seconds
+        timer_period = 0.05  # seconds
         self.joy = XboxController() #To control the robot using a game controller
-
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        # self.i = 0
 
     def timer_callback(self):
         LeftJoystickX,LeftTrigger,RightTrigger,A=self.joy.read()
         msg = Twist()
-        msg.linear.x=(float(RightTrigger)-float(LeftTrigger))/8
+        msg.linear.x=6*(float(RightTrigger)-float(LeftTrigger))/8
         msg.linear.y=0.
         msg.linear.z=0.
 
         msg.angular.x=0.
         msg.angular.y=0.
-        msg.angular.z=-float(LeftJoystickX)
+        msg.angular.z=-2*float(LeftJoystickX)
 
         self.publisher_.publish(msg)
-        self.get_logger().info('LeftJoystickX,LeftTrigger,RightTrigger,A: "%s"' % self.joy.read())
-        self.i += 1
+        # self.get_logger().info('LeftJoystickX,LeftTrigger,RightTrigger,A: "%s"' % self.joy.read())
+        # self.i += 1
 
 
 def main(args=None):
