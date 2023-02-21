357303af4e8d584237208d292311816d31016d30
Jakub Hodek <hodekjakub@gmail.com>
2023-02-21 10:29:35 +0100

    feat: Robot position detection using camera & Cinematic controller ("go to point")
    Closes #67

---
+++ ws/src/robot_control/robot_control/robot_position_controller.py
@@ -0,0 +1,80 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist, Pose2D
+import numpy as np
+
+
+class RobotPositionController(Node):
+
+    def __init__(self):
+        super().__init__('robot_position_controller')
+        self._publisher = self.create_publisher(Twist, 'demo/cmd_vel', 10)
+
+        self._subscription = self.create_subscription(
+            Pose2D, 'robot/position', self._new_position_received_callback, 10)
+        self._subscription  # prevent unused variable warning
+
+    def _new_position_received_callback(self, pose):
+        position = (pose.x, pose.y)
+        heading = pose.theta
+        # timestamp = 0
+        # print("new position received", position, heading)
+
+        target_position = (400, 400)  #  [x, y]
+
+        #  Compute the desired heading
+        desired_heading = np.arctan2(target_position[1] - position[1], target_position[0] - position[0])
+
+        #  Compute the distance to the target
+        distance_to_target = np.sqrt((target_position[1] - position[1]) ** 2 + (target_position[0] - position[0]) ** 2)
+
+        # desired_heading = np.pi  #  @TODO: set desired heading
+        self._heading_controller(desired_heading, heading, distance_to_target)
+
+    def __sawtooth(self, x):
+        return 2 * np.arctan(np.tan(x / 2))
+
+    def _heading_controller(self, desired_heading, heading, distance_to_target):
+        MAX_SPEED = 4.0
+
+        u_x = distance_to_target / 70.0
+        if abs(u_x) > MAX_SPEED:
+            u_x = MAX_SPEED * np.sign(u_x)
+
+        msg = Twist()
+        msg.linear.x = float(u_x)
+        msg.linear.y = 0.
+        msg.linear.z = 0.
+
+        msg.angular.x = 0.
+        msg.angular.y = 0.
+        u_z = -1 * self.__sawtooth(desired_heading - heading)
+        msg.angular.z = u_z  #  @ TODO: timestamp verification??
+        #  [-] .. clockwise rotation; [+] .. counter-clockwise rotation
+
+        # Stop the robot - if he is close enough to the target
+        # @ TODO OR if the position timestamp is too old
+        if distance_to_target < 5:
+            msg.linear.x = 0.
+            msg.angular.z = 0.
+        # msg.linear.x = 0.
+        # msg.angular.z = 0.
+
+        self._publisher.publish(msg)
+        self.get_logger().info('heading/desired: "%s"/"%s"; err: "%s" => u_z = %s; u_x = %s' %
+                               (heading, desired_heading, (desired_heading - heading), u_z, u_x))
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    robot_detector_publisher = RobotPositionController()
+
+    rclpy.spin(robot_detector_publisher)
+
+    robot_detector_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
---
+++ ws/src/top_camera/top_camera/robot_detector.py
@@ -0,0 +1,101 @@
+import cv2 as cv
+import numpy as np
+from rclpy.node import Node
+from zenith_camera_subscriber import *
+from tools.hsv_range_finder import HSVRangeFinder
+
+
+class RobotDetector(Node):
+
+    def __init__(self, data_updated_callback, debug=False, display_camera=False):
+        self._position = [0, 0]
+        self._heading = 0
+        self._timestamp = 0
+        self._terrain = None
+        self._hsvFinder = None
+        self._Camera = ZenithCameraSubscriber(self._camera_data_updated_callback)
+        self.data_updated_callback = data_updated_callback
+        self.__debug = debug
+        self.__display_camera = display_camera
+        rclpy.spin(self._Camera)
+
+    def _camera_data_updated_callback(self, img, timestamp):
+        self._terrain = img
+
+        # # GUI for finding the correct HSV range
+        # if (self.hsvFinder is None):
+        #     # Create an instance of the ColorThreshold class with the image
+        #     self.hsvFinder = HSVRangeFinder(self.terrain)
+        #     self.hsvFinder.run()
+
+        #  Detect the markers on the robot
+        blue_marker = [
+            (116, 200, 88),
+            (137, 255, 175)
+        ]
+        # green_marker = [
+        #     (39, 0, 77),
+        #     (63, 255, 149)
+        # ]
+        red_marker = [
+            (0, 35, 67),
+            (10, 255, 202)
+        ]
+        front_m_pos = self._detect_marker(blue_marker[0], blue_marker[1])
+        rear_m_pos = self._detect_marker(red_marker[0], red_marker[1])
+
+        if self.__debug:
+            print("Front marker: ", front_m_pos)
+            print("Rear marker: ", rear_m_pos)
+
+        #  If both markers are detected, calculate the position and heading of the robot
+        if (front_m_pos[0] is not None and rear_m_pos[0] is not None):
+            self._heading = np.arctan2(front_m_pos[1] - rear_m_pos[1], front_m_pos[0] - rear_m_pos[0])
+            self._position = [rear_m_pos[0], rear_m_pos[1]]  #  TODO: change to center of robot
+            self._timestamp = timestamp
+
+            self.data_updated_callback(self._position, self._heading, self._timestamp)
+            # self._heading_controller(np.pi)  #  TODO: change to desired angle
+            if self.__debug:
+                print('angle', np.rad2deg(self._heading))
+
+        if self.__display_camera:
+            cv.imshow('Terrain', self._terrain)
+            # cv.imwrite('terrain.png', self._terrain)
+            cv.waitKey(1)
+
+    def _detect_marker(self, hsv_min=(24, 1, 1), hsv_max=(51, 255, 255)):
+        frame_HSV = cv.cvtColor(self._terrain, cv.COLOR_BGR2HSV)
+        frame_threshold = cv.inRange(frame_HSV, hsv_min, hsv_max)
+        ret, thresh = cv.threshold(frame_threshold, 127, 255, cv.THRESH_BINARY)
+        contours, h = cv.findContours(thresh, cv.RETR_CCOMP,
+                                      cv.CHAIN_APPROX_NONE)
+
+        n_contours = len(contours)
+
+        if n_contours == 1:
+            cnt = contours[0]
+            # find circle
+            (cx, cy), radius = cv.minEnclosingCircle(cnt)
+            cv.circle(self._terrain, (int(cx), int(cy)), int(radius), (0, 255, 0), 1)  # draw circle
+
+            return (cx, cy)
+        else:
+            return (None, None)  #  No marker / multiple markers found
+
+    def get_heading(self):
+        return self._heading
+
+    def get_position(self):
+        return self._position
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    rd = RobotDetector(debug=True)
+    rd.Camera.destroy_node()
+
+
+if __name__ == '__main__':
+    main()
